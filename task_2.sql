6e238dd2c4e433473c8622a0186c9b8a777ec89b
Jonathan Lartey <lightlartey2000@gmail.com>
2025-07-03 23:34:35 +0000

    task 2

--- task_2.sql
+++ task_2.sql
@@ -9,9 +9,25 @@ database = 'alx_book_store'
 
 cursor = alx_book_store.cursor()
 cursor.execute ("""
-CREATE TABLE IF NOT EXISTS Authors(
-	author_id INT AUTO_INCREMENT PRIMARY KEY, author_name VARCHAR(215)
-)
+    CREATE TABLE IF NOT EXISTS Authors(
+        author_id INT AUTO_INCREMENT PRIMARY KEY,
+        author_name VARCHAR(215)
+    )
+""")
+
+cursor.close()
+alx_book_store.close()
+
+cursor = alx_book_store.cursor()
+cursor.execute ("""
+    CREATE TABLE IF NOT EXISTS Books(
+        book_id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(130),
+        author_id INT,
+        price DOUBLE,
+        publication_date DATE,
+        FOREIGN KEY (author_id) REFERENCES Authors (authors_id)
+    )
 """)
 
 cursor.close()
