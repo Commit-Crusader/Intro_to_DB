158919a986861ec61c007cc7ab5d02f3cc0c5e35
Jonathan Lartey <lightlartey2000@gmail.com>
2025-07-04 00:39:29 +0000

    task 4

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,6 @@
 USE alx_book_store;
 
-SELECT * FROM informaton_schema.columns
+SELECT COLUMN_NAME, COLUMN_TYPE,
+FROM informaton_schema.columns
 WHERE table_schema = 'alx_book_store'
     AND table_name = 'Books';
