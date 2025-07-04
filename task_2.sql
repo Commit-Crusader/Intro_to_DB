527db5743438ac6da7aa0c02f3734b9cf3ff8662
Jonathan Lartey <lightlartey2000@gmail.com>
2025-07-04 00:11:54 +0000

    task 2

--- task_2.sql
+++ task_2.sql
@@ -54,7 +54,7 @@ cursor.execute ("""
         book_id INT,
         quantity DOUBLE,
         FOREIGN KEY (book_id) REFERENCES Books(book_id),
-        FOREIGN KEY (order_id) REFERENCES Orders (order_id)
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id)
     )
 """)
 
